a0a6de350105ffcf99a4a7f260413213f37dd92d
İbrahim İpek <40497100+CprogrammerIbrahim@users.noreply.github.com>
2020-01-01 14:15:07 +0300

    Update Autocorrect.py

--- Autocorrect/Autocorrect.py
+++ Autocorrect/Autocorrect.py
@@ -1,12 +1,13 @@
 from Levenshtein import distance
 from wordfreq import get_frequency_dict
 
-diction = get_frequency_dict('en')
-
-#Returns a list of Autocorrect proposals for x
-def Autocorrect(x):
-     neighbourhood = {} #Frequency dict. of words near to x
-     for word in diction.keys():
-         if distance(x, word) < 3: 
-            neighbourhood[word] = diction[word]
-     return sorted(neighbourhood, key=neighbourhood.get, reverse=True)[:3] #First 3 'near' words with highest frequency of use
+class Autocorrect(object):
+     def __init__(self, language='en'):
+          self.diction = get_frequency_dict(language)
+          
+     def propose(self, x):
+          neighbourhood = {} #Frequency dict. of words near to x
+          for word in self.diction.keys():
+               if distance(x, word) < 3: 
+                    neighbourhood[word] = self.diction[word]
+          return sorted(neighbourhood, key=neighbourhood.get, reverse=True)[:3] 
